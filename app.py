6bf164633dc4ff60dc52de56fbfd7988d1517aa1
tgvnct <tgvnct@gmail.com>
2025-07-03 09:31:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ system_instruction = (
 )
 
 model = genai.GenerativeModel(
-    model_name="gemini-pro",
+    model_name="models/gemini-1.5-flash-latest",
     system_instruction=system_instruction
 )
 
